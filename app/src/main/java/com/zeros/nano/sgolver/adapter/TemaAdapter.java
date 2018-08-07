46f6bd93343a344a2be2b7675316cf6b9003a6dd
erwin <erwin.plaza.x@gmail.com>
2018-08-07 12:04:37 -0400

    Adicion de la clase adapter para los temas

---
+++ app/src/main/java/com/zeros/nano/sgolver/adapter/TemaAdapter.java
@@ -0,0 +1,11 @@
+package com.zeros.nano.sgolver.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+public class TemaAdapter extends ArrayAdapter {
+    public TemaAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull Object[] objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
+}
